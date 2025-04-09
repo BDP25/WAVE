b18ef4f9cc77c70c750e3dd450be307e483f726a
eliashager <hagereli@students.zhaw.ch>
2025-04-09 11:29:19 +0200

    run application small ajdustment

--- data_collector/run.py
+++ data_collector/run.py
@@ -1,7 +1,21 @@
 import datetime
+import time
+
 from get_news_data import fetch_swissdox_data
 
 from clean_data import clean_and_process_data
+from load_db import load_data as db_load
+from clustering import df_plot_dbscan_with_json_output, load_data
+import os
+
+
+db_params = {
+        "dbname": os.getenv("DB_NAME", "your_database"),
+        "user": os.getenv("DB_USER", "your_username"),
+        "password": os.getenv("DB_PASSWORD", "your_password"),
+        "host": os.getenv("DB_HOST", "localhost"),
+        "port": os.getenv("DB_PORT", "5432")
+    }
 
 # Calculate the date range for the last week
 end_date = datetime.date.today()
@@ -11,6 +25,16 @@ start_date = end_date - datetime.timedelta(days=2)
 # Format the dates in the desired format (YYYY-MM-DD)
 start_date_str = start_date.strftime("%Y-%m-%d")
 end_date_str = end_date.strftime("%Y-%m-%d")
-
+raw_data_folder = './raw_data'
+os.makedirs(raw_data_folder, exist_ok=True)
 fetch_swissdox_data(start_date_str, end_date_str)
+cleaned_data_folder = './cleaned_data'
+os.makedirs(cleaned_data_folder, exist_ok=True)
 clean_and_process_data()
+
+time.sleep(2)
+df = load_data()
+json_data = df_plot_dbscan_with_json_output(df, target_clusters=(4, 6))
+db_load(json_data, db_params)
+
+
