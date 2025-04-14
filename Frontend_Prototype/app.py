bad080dcf703457200cb7142b88620f56a9807f9
eliashager <hagereli@students.zhaw.ch>
2025-04-14 16:32:38 +0200

    add API endpoint to retrieve article history by title

--- Frontend_Prototype/app.py
+++ Frontend_Prototype/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, render_template, request
 from frontend_agregator import get_clusters_per_date, get_min_max_date
-
+from db_utils import get_article_history_by_title
 app = Flask(__name__)
 
 
@@ -21,6 +21,22 @@ def api_clusters():
         return jsonify(clusters)
     except Exception as e:
         print(f"Fehler in api_clusters: {str(e)}")
+        return jsonify({"error": str(e)}), 500
+
+
+@app.route("/api/article_history")
+def api_article_history():
+    try:
+        article_title = request.args.get("title")
+        # TODO debug
+        print(article_title)
+        article_title = "Refugiados"
+        if not article_title:
+            return jsonify({"error": "Kein Artikel angegeben"}), 400
+        history = get_article_history_by_title(article_title)
+        return jsonify(history)
+    except Exception as e:
+        print(f"Fehler in api_article_history: {str(e)}")
         return jsonify({"error": str(e)}), 500
 
 
