2abc7d8bdfb76222c738a2ca3504931f391b8f5e
Flühmann Simeon (fluehsi2) <fluehsi2@students.zhaw.ch>
2025-05-21 11:34:08 +0200

    init refactor

--- Frontend_Prototype_NET/vis_text_div/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Wiki revision diff visualization module.
-"""
-from .visualization import visualize_wiki_versions_with_deletions
-from .db_history import get_revisions_by_article_id, get_revisions_between
-from .extractor import extract_revision_texts
